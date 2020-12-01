a00f7c5477f72e75bb8ef725a186bab1f30911e8
ZacharyChen <zacharyzekaichen@gmail.com>
2020-12-01 08:15:26 +0800

    String v2 20201201

--- String/src/StringMethodTest.java
+++ String/src/StringMethodTest.java
@@ -1,12 +1,53 @@
 import org.junit.Test;
 
 import javax.sound.midi.Soundbank;
+import java.util.Arrays;
 
 /**String方法练习
  * @author chenzk
  * @create 2020-12-01 7:05
  */
 public class StringMethodTest {
+
+    @Test
+    public void testStringMethod(){
+        String str = "abcdefghijklmn";
+        int length = str.length();
+        char c = str.charAt(0);
+        boolean empty = str.isEmpty();
+        String strUpper = str.toUpperCase(); //创建新的字符串，而不是在原来基础上全部转换为大写。
+        String strLower = str.toLowerCase();
+        String strTrim = str.trim(); //去除首尾空格
+        boolean equal1 = str.equals("abc");
+        boolean equal2 = str.equalsIgnoreCase("abc");
+        int compare = str.compareTo("abr"); //注意内部是如何实现的，如果遇到不同的字符则相减，从头到尾都没遇到不同的则长度相减。
+        String substring = str.substring(0, 3);
+
+        boolean end1 = str.endsWith("jklmn");
+        boolean start1 = str.startsWith("abc");
+        boolean start2 = str.startsWith("efg", 3);
+        boolean start3 = str.startsWith("efg", 4);
+        System.out.println(start2 + " " + start3); //false true
+
+        boolean contain = str.contains("abc");
+        //含多个子字符串的时候可用上
+        int index1 = str.indexOf("efg");
+        int index2 = str.indexOf("efg", 4);
+        int index3 = str.lastIndexOf("efg");
+        int index4 = str.lastIndexOf("efg", 6);
+
+        String str2 = str.replace("abc", "123");
+
+        //与字符数组的转换
+        char[] chars = str.toCharArray();
+        System.out.println(Arrays.toString(chars));
+        String str3 = new String(chars);
+        String str5 = String.valueOf(chars);
+        //与字节数组的转换
+        byte[] bytes = str.getBytes();
+        System.out.println(Arrays.toString(bytes));
+        String str4 = new String(bytes);
+    }
 
     /**
      * 通过以下情况说明，变量加上字符串赋值的方式是通过new方式创建String对象的。
